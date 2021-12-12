04cbfc4a839c16f773f9a63d3af4903a77aa1682
Metalhammer76 <95860404+Metalhammer76@users.noreply.github.com>
2021-12-12 19:22:29 +0100

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,21 @@
+package Ejercicio1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        /* Tipos de datos :
+           A.-Primitivos:
+                        *Numéricos .Enteros:byte,short,integer,long
+                                   .Decimales:float,double
+                        *Booleanos:true,false
+                        *Caracteres:character
+          B.-Estructurados:
+                         *Cadenas:string
+                         *Vectores:array
+                         *Clases:biblioteca java,de terceros,propias
+          C.-Envoltorio
+        */
+        System.out.println("Esto es para comerse los muñones");
+
+    }
+}
